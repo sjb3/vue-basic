d5323de6675b40a6dce1515515f9062af967a794
Sung J Byun <sungjbyun@D-69-91-176-37.dhcp4.washington.edu>
2016-12-12 19:20:19 -0800

    fixed stupid errors

--- myapp/src/components/Users.vue
+++ myapp/src/components/Users.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="users">
         <h1>Users</h1>
+        <form v-on:submit="addUser">
+            <input type="text" v-model="newUser.name" placeholder="Enter Name" />
+            <br />
+            <input type="text" v-model="newUser.email" placeholder="Enter Email" />
+            <br />
+            <input type="submit" value="Submit">
+        </form>
+        <ul>
+             <li v-for="user in users">
+                 {{user.name}}: {{user.email}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,18 +22,33 @@
         name: 'users',
         data() {
             return {
+                newUser: {},
                 users: [
                     {
-
-                    }
+                        name: 'Jane Doe',
+                        email: 'janedoe@mymail.com',
+                        contacted: false
+                    },
+                    {
+                        name: 'Jane Doe1',
+                        email: 'janedoe1@mymail.com',
+                        contacted: false
+                    },
+                    {
+                        name: 'Jane Doe2',
+                        email: 'janedoe2@mymail.com',
+                        contacted: false
+                    },
                 ]
             }
         },
-        methods: {
-  
-        }
-       
-   }
+       methods: {
+           addUser: function(e){
+               console.log('add');
+               e.preventDefault();    
+           }
+        },
+  }
 </script>
 
 <style scoped>
