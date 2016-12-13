ddeab78a3e7099394ed7bce1b7791bc0fbe81415
Sung J Byun <sungjbyun@D-173-250-160-234.dhcp4.washington.edu>
2016-12-12 16:22:02 -0800

    no eslint, adding pic

---
+++ myapp/src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="test">
+         
+        <h1>{{title}}</h1>
+        <p v-if="showName">my name is : {{user.firstName}}</p>
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: 'hello Human!',
+                user: {
+                    firstName: 'Justin',
+                    lastName: 'Doe'
+                },
+                showName: true,
+                items: [
+                    {title: 'Item One'},
+                    {title: 'Item Two'},
+                    {title: 'Item Three'},
+                    {title: 'Item Four'}
+                ]
+            }
+        }
+   }
+</script>
+
+<style scoped>
+</style>
